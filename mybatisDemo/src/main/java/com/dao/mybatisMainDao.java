1a720e2206f7dbe4d51ddcd362195e7e9f9183c7
YANCOOO <642814178@qq.com>
2020-06-15 22:35:53 +0800

    6.15

--- mybatisDemo/src/main/java/com/dao/mybatisMainDao.java
+++ mybatisDemo/src/main/java/com/dao/mybatisMainDao.java
@@ -2,10 +2,32 @@ package com.dao;
 //持久层接口
 
 import com.source.mybatisMain;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 
 import java.util.List;
 
 public interface mybatisMainDao {
-//查询所有
+    //    查询
+    //    select last_insert_id查询最后插入的id 使用标签<selectkey order = after >
+    //    在插入数据后执行
+    @Select("SELECT * FROM user")
     List<mybatisMain> findAll();
+
+    //    插入
+    @Insert("INSERT INTO user(id,username,birthday,sex,adress) VALUES(#{id},#{username},#{birthday},#{sex},#{adress})")
+    void insertDb(mybatisMain mybatisMain);
+
+    //    更新
+    @Update("UPDATE user SET username=#{username} where id=#{id}")
+    void updateDb(mybatisMain mybatisMain);
+
+    //    删除
+    @Delete("DELETE FROM user WHERE id=#{id}")
+    void deleteDb(mybatisMain mybatisMain);
+
 }
+
--- mybatisDemo/src/test/java/dao/mybatisTest.java
+++ mybatisDemo/src/test/java/dao/mybatisTest.java
@@ -6,30 +6,81 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 
 public class mybatisTest {
-    public static void main(String[] args) throws IOException {
+    private InputStream in;
+    private SqlSession session;
+    private mybatisMainDao mybatisMainDao;
+@Before
+    public void init() throws IOException {
 //        读取配置信息
-        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
 //        创建sqlSessionFactory工厂
+//        构建者模式 把对象创建细节隐藏 使用者直接调用方法即可拿到对象
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+//        工厂模式 优势解耦
         SqlSessionFactory factory = builder.build(in);
 //        使用工厂生产sqlSession创建Dao接口代理对象
-        SqlSession session = factory.openSession();
+        session = factory.openSession();
 //        使用SqlSession创建Dao接口的代理对象mybatisMain
-        mybatisMainDao mybatisMainDao = session.getMapper(mybatisMainDao.class);
+//        mapper代理模式 不修改源码的基础上对已有方法增强
+        mybatisMainDao = session.getMapper(mybatisMainDao.class);
+    }
+@After
+    public void destroy() throws Exception {
+        //        提交事务
+        session.commit();
+        //        释放资源
+        session.close();
+        in.close();
+    }
+
+    @Test
+    public void selectAll() throws Exception {
 //        代理对象执行方法
         List<mybatisMain> users = mybatisMainDao.findAll();
         for (mybatisMain mybatisMain1 : users) {
             System.out.println(mybatisMain1);
         }
-//        释放资源
-        session.close();
-        in.close();
     }
+
+    @Test
+    public void insertDb() {
+        mybatisMain mybatisMain = new mybatisMain();
+        mybatisMain.setId(6);
+        mybatisMain.setUsername("老六");
+        mybatisMain.setBirthday(new Date());
+        mybatisMain.setSex("男");
+        mybatisMain.setAdress("大连");
+
+        mybatisMainDao.insertDb(mybatisMain);
+    }
+
+    @Test
+    public void updateDb() {
+        mybatisMain mybatisMain = new mybatisMain();
+        mybatisMain.setId(6);
+        mybatisMain.setUsername("老6");
+
+        mybatisMainDao.updateDb(mybatisMain);
+    }
+
+
+    @Test
+    public void deleteDb() {
+        mybatisMain mybatisMain = new mybatisMain();
+        mybatisMain.setId(6);
+
+        mybatisMainDao.deleteDb(mybatisMain);
+    }
+
 }
